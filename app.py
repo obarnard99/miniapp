f41d2c84418a24e5204225c4e42329501e3ffcaa
Ollie Barnard <obarnard99@gmail.com>
2020-07-06 10:42:57 +0100

    Return host name

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Running on %s' % request.host
 
 
 if __name__ == '__main__':

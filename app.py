6aa0c728bac2924f41b8ca7a5f8599478228ba15
Ollie Barnard <obarnard99@gmail.com>
2020-07-09 16:35:34 +0100

    Add -p flag to change port and report back

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Running on %s' % request.host
-
+def show_port():
+    return "Running on port %s" % app.config['port']
 
 if __name__ == '__main__':
-    app.run()
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('-p', default=5000, type=int)
+  args = parser.parse_args()
+  port = args.p
+  app.config['port'] = port
+  app.run(port=port)

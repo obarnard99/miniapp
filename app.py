a3dabc6f309c99ce50b96a7544c29d8a8c76ab86
Ollie Barnard <obarnard99@gmail.com>
2020-08-10 14:17:21 +0100

    Add host to args

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def show_port():
-    return "Running on port %s" % app.config['port']
+    return f"Running on {app.config['host']}:{app.config['port']}"
 
 if __name__ == '__main__':
   from argparse import ArgumentParser
-  parser = ArgumentParser()
+  parser = ArgumentParser(add_help=False)
   parser.add_argument('-p', default=5000, type=int)
+  parser.add_argument('-h', default='0.0.0.0', type=str)
   args = parser.parse_args()
   port = args.p
+  host = args.h
   app.config['port'] = port
-  app.run(host='0.0.0.0',port=port)
+  app.config['host'] = host
+  app.run(host=host,port=port)

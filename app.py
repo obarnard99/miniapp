00d49ecb0a05ba4eeb6ca4d4c7c79e725a68af16
Ollie Barnard <obarnard99@gmail.com>
2020-08-18 14:16:07 +0100

    Optional SSL

--- app.py
+++ app.py
@@ -11,9 +11,14 @@ if __name__ == '__main__':
   parser = ArgumentParser(add_help=False)
   parser.add_argument('-p', default=5000, type=int)
   parser.add_argument('-h', default='0.0.0.0', type=str)
+  parser.add_argument('-s', default=False, type=bool)
   args = parser.parse_args()
   port = args.p
   host = args.h
+  ssl = args.s
   app.config['port'] = port
   app.config['host'] = host
-  app.run(host=host,port=port)
+  if ssl:
+    app.run(host=host, port=port, ssl_context='adhoc')
+  else:
+    app.run(host=host, port=port)
